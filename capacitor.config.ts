ce12ddc6596effed0ae5849105eeda620d7282e4
khan4lov <aayankhan31101995@gmail.com>
2026-01-09 12:44:48 +0530

    App icon and Spalash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,22 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.serviceoncall.app',
-  appName: 'MyServiceApp',
+  appName: 'Service On Call',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000, // Shows splash for 2 seconds
+      launchAutoHide: true,
+      backgroundColor: "#ffffff", // Change to your brand color (e.g., #000000 for black)
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
